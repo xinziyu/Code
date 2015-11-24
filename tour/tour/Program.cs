efef13b5ed683100f3e68d1cdbfc6d11a833ac2d
xuyang <xinziyujob@163.com>
2015-11-24 20:38:45 +0800

    我是master第三个版本

--- tour/tour/Program.cs
+++ tour/tour/Program.cs
@@ -10,7 +10,7 @@ namespace tour
         static void Main(string[] args)
         {
 
-            string msg = "我是master第一个版本";
+            string msg = "我是master第三个版本";
             Console.WriteLine(msg);
 
 
