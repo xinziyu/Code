0aa76aa9c0fec7f29d4ea807a50e188907a2d7c4
xuyang <xinziyujob@163.com>
2015-11-24 22:13:52 +0800

    0000000

--- tour/tour/Program.cs
+++ tour/tour/Program.cs
@@ -9,9 +9,9 @@ namespace tour
     {
         static void Main(string[] args)
         {
-            string msg = "我是dev第1个版本";
+            string msg = "我是dev第2个版本";
             Console.WriteLine(msg);
-            Console.WriteLine("123");
+            Console.WriteLine("5555");
             Console.WriteLine("ok");
             Console.ReadKey();
         }
